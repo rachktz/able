a6e99e7f67d6ac6b48189b1b75ad3572f0ccea20
Rachel Katz <rachelleorakatz@gmail.com>
2015-05-13 23:16:23 -0700

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,9 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  # Required for Devise
+  config.action_mailer.default_url_options = { :host => 'http://pinteresting-commits.herokuapp.com/' }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
